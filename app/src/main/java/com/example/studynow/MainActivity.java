6f3dd666ea4004061bff49fcd042dfbde964c8a1
khanhngoc2009 <khanhpvph10443@fpt.edu.vn>
2020-06-14 23:17:14 +0700

    sửa lại

--- app/src/main/java/com/example/studynow/MainActivity.java
+++ app/src/main/java/com/example/studynow/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this,Activity_Menu_Toan.class);
+                Intent intent = new Intent(MainActivity.this,Activity_Menu.class);
                 startActivity(intent);
                 finish();
             }
