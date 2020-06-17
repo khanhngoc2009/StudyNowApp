b71a732b57183f41efc2904378f0d3ae7fce5945
khanhngoc2009 <khanhpvph10443@fpt.edu.vn>
2020-06-18 00:34:24 +0700

    thêm toan.ly,hoa,van,anh

--- app/src/main/java/com/example/studynow/Activity_Menu.java
+++ app/src/main/java/com/example/studynow/Activity_Menu.java
@@ -14,10 +14,42 @@ public class Activity_Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__menu);
         btn_toan=(Button) findViewById(R.id.btn_toan);
+        btn_ly=(Button) findViewById(R.id.btn_ly);
+        btn_hoa=(Button) findViewById(R.id.btn_hoa);
+        btn_van=(Button) findViewById(R.id.btn_van);
+        btn_anh=(Button) findViewById(R.id.btn_anh);
         btn_toan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(Activity_Menu.this,Activity_Menu_Toan.class);
+                startActivity(intent);
+            }
+        });
+        btn_ly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Activity_Menu.this,Activity_Menu_Vatly.class);
+                startActivity(intent);
+            }
+        });
+        btn_hoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Activity_Menu.this,Activity_Menu_Hoa.class);
+                startActivity(intent);
+            }
+        });
+        btn_van.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Activity_Menu.this,Activity_Menu_Vanhoc.class);
+                startActivity(intent);
+            }
+        });
+        btn_anh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Activity_Menu.this,Activity_Menu_AnhVan.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/studynow/Activity_Menu_AnhVan.java
@@ -0,0 +1,14 @@
+package com.example.studynow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Menu_AnhVan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__menu__anh_van);
+    }
+}
